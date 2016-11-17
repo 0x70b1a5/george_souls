736ddfcfd644248f1b8d3cebfeebe50b0d407ee4
Tobias Merkle <tgamerkle@gmail.com>
2016-11-16 21:51:37 -0800

    hero and hero sprite

--- george_souls.py
+++ george_souls.py
@@ -1,19 +1,30 @@
 import sys
 import pygame
+import pdb
+from settings import Settings
+from hero import Hero
 
 def run():
 	"""Init game and create screen object."""
 	pygame.init()
-	screen = pygame.display.set_mode((1200,800))
+	sets = Settings()
+	screen = pygame.display.set_mode(
+		(sets.screen_width, sets.screen_height))
 	pygame.display.set_caption("George Souls")
+
+	# Create hero
+	hero = Hero(screen)
+
 	# Main game loop
 	while True:
 		# Listen kb, mouse
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
+		# Redraw screen
+		hero.blitme()
 
-		# Make msot recently drawn screen visible
+		# Render(?) screen
 		pygame.display.flip()
 
 run()
